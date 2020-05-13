f934b5b9c4f6cbc5a33edb6d0aada9c54c80a030
scuri <scuri@sourceforge>
2020-05-13 21:52:15 +0000

    Jasper 2.0.14
    git-svn-id: https://svn.code.sf.net/p/imtoolkit/im/trunk/im@769 af3c9e47-74de-4ca0-9433-524f997074d0

---
+++ src/libjasper2/jasper/jas_config.h
@@ -0,0 +1,47 @@
+/* IMLIB - jas_config.h for IM */
+
+/* Avoid problems due to multiple inclusion. */
+#ifndef JAS_CONFIG_H
+#define JAS_CONFIG_H
+
+#include <jasper/jas_dll.h>
+
+/* This preprocessor symbol identifies the version of JasPer. */
+#define JAS_VERSION "2.0.14"
+
+#define JAS_HAVE_FCNTL_H		1
+#define JAS_HAVE_IO_H			1
+#define JAS_HAVE_WINDOWS_H		1
+#define JAS_HAVE_SYS_TYPES_H	1
+
+#undef JAS_HAVE_UNISTD_H
+#undef JAS_HAVE_SYS_TIME_H
+#undef JAS_HAVE_GETTIMEOFDAY
+#undef JAS_HAVE_GETRUSAGE
+#undef JAS_HAVE_SNPRINTF
+
+// I don't think that this should be needed anymore.
+//#if 1
+//#ifndef __cplusplus
+//#undef inline
+//#define inline __inline
+//#endif
+//#endif
+
+#define ssize_t long long
+
+#if !defined(JAS_DEC_DEFAULT_MAX_SAMPLES)
+#define JAS_DEC_DEFAULT_MAX_SAMPLES (64 * ((size_t) 1048576))
+#endif
+
+#if defined(__GNUC__) && !defined(__clang__)
+#define JAS_ATTRIBUTE_DISABLE_USAN \
+  __attribute__((no_sanitize_undefined))
+#elif defined(__clang__)
+#define JAS_ATTRIBUTE_DISABLE_USAN \
+  __attribute__((no_sanitize("undefined")))
+#else
+#define JAS_ATTRIBUTE_DISABLE_USAN
+#endif
+
+#endif
