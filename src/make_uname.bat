68d5e0941db9a12ac49f90e08acb582889ad5671
scuri <scuri@sourceforge>
2020-07-07 21:52:14 +0000

    Support for Lua 5.4
    git-svn-id: https://svn.code.sf.net/p/imtoolkit/im/trunk/im@802 af3c9e47-74de-4ca0-9433-524f997074d0

--- src/make_uname.bat
+++ src/make_uname.bat
@@ -21,3 +21,4 @@ REM call make_uname_lua %1 %2 %3 %4 %5 %6 %7 %8
 call make_uname_lua %1 "USE_LUA51=Yes" %2 %3 %4 %5 %6 %7 %8
 call make_uname_lua %1 "USE_LUA52=Yes" %2 %3 %4 %5 %6 %7 %8
 call make_uname_lua %1 "USE_LUA53=Yes" %2 %3 %4 %5 %6 %7 %8
+call make_uname_lua %1 "USE_LUA54=Yes" %2 %3 %4 %5 %6 %7 %8
